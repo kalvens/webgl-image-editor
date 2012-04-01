349ff19d25535206cb6a59b2d5a1bcccd28b97f8
swiese <thewec@hotmail.com>
2012-04-01 15:39:30 -0600

    First Commit

---
+++ GraphicsProject/src/com/seanwiese/graphics/GraphicsProjectServlet.java
@@ -0,0 +1,13 @@
+package com.seanwiese.graphics;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class GraphicsProjectServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+	    throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
